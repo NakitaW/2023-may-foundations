61e1607c27c6568453b6f66daa58402342f842aa
NakitaW <nakita_w@ymail.com>
2023-08-15 22:24:55 -0500

    Final Project

--- Foundations Final Project/setup.js
+++ Foundations Final Project/setup.js
@@ -1,1 +1,14 @@
+$(document).ready(function() {
+  $("#subscription-form").submit(function(e) {
+    e.preventDefault();
 
+    var email = $("#email").val();
+
+    // Perform validation, subscription process, and display message
+    if (email !== "") {
+      $("#message").text("Subscribed successfully!");
+    } else {
+      $("#message").text("Please enter a valid email.");
+    }
+  });
+});
